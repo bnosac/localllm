42b9b2c2558b400c236ca0b7f4783bdbd203da62
Jan Wijffels <jwijffels@bnosac.be>
2026-03-27 13:04:51 +0100

    export localllm_connect

--- src/localllm/__init__.py
+++ src/localllm/__init__.py
@@ -2,12 +2,15 @@
 localllm: Run a local LLM alongside dspy
 """
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 
 from .dspy import LocalLLM, LocalChatAdapter, dspy_signature_defaults
 from .models import localllm_download_model
 from .utils import txt_locate_all, txt_locate, merge_spans
+from .config import localllm_connect
 
-__all__ = ["LocalLLM", "localllm_download_model", "LocalChatAdapter", "dspy_signature_defaults",
-           "txt_locate_all", "txt_locate", "merge_spans"]
+__all__ = [
+    "localllm_connect",
+    "LocalLLM", "localllm_download_model", "LocalChatAdapter", "dspy_signature_defaults",
+    "txt_locate_all", "txt_locate", "merge_spans"]
