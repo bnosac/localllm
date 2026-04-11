a18f5bfa27f802cbba651f44aa431b04fbd06ea5
Jan Wijffels <jwijffels@bnosac.be>
2026-04-11 23:03:44 +0200

    export tif

--- src/localllm/__init__.py
+++ src/localllm/__init__.py
@@ -9,8 +9,10 @@ from .dspy import LocalLLM, LocalChatAdapter, dspy_signature_defaults
 from .models import localllm_download_model
 from .utils import txt_locate_all, txt_locate, merge_spans
 from .config import localllm_connect
+from .converters import tif
 
 __all__ = [
     "localllm_connect",
     "LocalLLM", "localllm_download_model", "LocalChatAdapter", "dspy_signature_defaults",
-    "txt_locate_all", "txt_locate", "merge_spans"]
+    "txt_locate_all", "txt_locate", "merge_spans",
+    "tif"]
