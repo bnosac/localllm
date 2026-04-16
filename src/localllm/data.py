703c9635342efd1a91346059572e751b40d8a8e9
Jan Wijffels <jwijffels@bnosac.be>
2026-04-16 15:21:01 +0200

    no need polars

--- src/localllm/data.py
+++ src/localllm/data.py
@@ -1,8 +1,11 @@
+import csv
+import zipfile
 import importlib_resources
-from zipfile import ZipFile
-import polars as pl
+from typing import List
+from io import StringIO
+#from zipfile import ZipFile
 
-def data_be_parliament() -> pl.DataFrame:
+def data_be_parliament() -> List[dict]:
     """
     Dataset from 2017 with Questions asked by members of the Belgian Federal Parliament and the Answers provided to these questions.
     The dataset was extracted from http://data.dekamer.be and contains questions asked by persons in the Belgium Federal parliament and answers given by the departments of the Federal Belgian Ministers.
@@ -26,10 +29,10 @@ def data_be_parliament() -> pl.DataFrame:
     Returns
     --------
 
-    pl.DataFrame
-        A polars DataFrame with example data.
-        The dataframe has 14034 rows with 7017 questions which are both available in french and dutch. 
-        The dataframe has the following columns: 'doc_id', 'depotdat', 'aut_party', 'aut_person', 'aut_language', 'language', 'question', 'question_theme_main', 'question_theme', 'answer', 'answer_deptpres', 'answer_department', 'answer_subdepartment'
+    list[dict]
+        A list of dicts where each list element is a question asked in parliament.
+        The dataset has 14034 rows with 7017 questions which are both available in french and dutch. 
+        The dataset has the following columns: 'doc_id', 'depotdat', 'aut_party', 'aut_person', 'aut_language', 'language', 'question', 'question_theme_main', 'question_theme', 'answer', 'answer_deptpres', 'answer_department', 'answer_subdepartment' if you convert it to pandas or polars
 
     Reference
     ---------
@@ -41,8 +44,35 @@ def data_be_parliament() -> pl.DataFrame:
 
     >>> from localllm.data import data_be_parliament
     >>> x = data_be_parliament()
+    >>> import pprint
+    >>> pprint.pprint(x[0], width=1000)                                       # doctest: +SKIP
+    {
+      'answer': 'Ik nodig het geachte lid uit zijn vraag te stellen aan mijn collega Kris Peeters, minister van Werk en Economie (Vraag nr. 2000 van 14 februari 2018).',        
+      'answer_department': "Minister van Middenstand, Zelfstandigen, KMO's, Landbouw en Maatschappelijke Integratie",
+      'answer_deptpres': '14',
+      'answer_subdepartment': "Middenstand, Zelfstandigen, KMO's, Landbouw en Maatschappelijke Integratie",
+      'aut_language': 'french',
+      'aut_party': 'MR',
+      'aut_person': 'Flahaux, Jean-Jacques',
+      'depotdat': '2017-12-29',
+      'doc_id': 'http://data.dekamer.be/v0/qrva/54-B144-14-1021-2017201819553',
+      'language': 'dutch',
+      'question': 'Percentage vrouwen met een eenoudergezin. \r\n\r\n In Wallonie werden de eenoudergezinnen onlangs gescreend. Daaruit is gebleken dat eenoudergezinnen vandaag 12,20 % van de gezinnen in het zuiden van het land vertegenwoordigen en dat het in acht op de tien gevallen over alleenstaande vrouwen gaat.Hebt u, ter aanvulling van die informatie, cijfers voor het hele land? Zo ja, kunt u dan ook in voorkomend geval de cijfers voor het verleden meedelen, zodat ik mij een beeld kan vormen van de evolutie van de problematiek?',
+      'question_theme': 'GEZIN  |  ALLEENSTAANDE  |  VROUW',
+      'question_theme_main': ''
+    }
+    >>> 
+    >>> import polars as pl                        # doctest: +SKIP
+    >>> be = pl.from_records(x)                    # doctest: +SKIP
+    >>> import pandas as                           # doctest: +SKIP
+    >>> be = pd.DataFrame.from_records(x)          # doctest: +SKIP
     """
     path = importlib_resources.files("localllm") / "data" / "be_parliament.zip"        
     path = str(path)
-    x = pl.read_csv(ZipFile(path).open("be_parliament.csv", mode='r').read(), encoding = "utf-8", infer_schema=False)
+    #x = pl.read_csv(ZipFile(path).open("be_parliament.csv", mode='r').read(), encoding = "utf-8", infer_schema=False)
+    with zipfile.ZipFile(path, 'r') as zip_ref:
+        with zip_ref.open("be_parliament.csv") as csv_file:
+            csv_content = csv_file.read().decode('utf-8')
+            reader = csv.DictReader(StringIO(csv_content))
+            x = [row for row in reader]
     return x
